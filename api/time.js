f5055feb12aaf7d652b3e92b057c1148a0a598ed
msmartstudiodev <contact.msmartstudio.dev@gmail.com>
2025-09-13 13:18:22 +0530

    time.js

---
+++ api/time.js
@@ -0,0 +1,8 @@
+export default function handler(request, response) {
+  const currentTime = new Date();
+
+  response.status(200).json({
+    time: currentTime.toISOString(),
+    timezone: 'UTC',
+  });
+}
